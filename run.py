a81bbef67937e5c68753f55086301b4cc9cb17f0
ThisIsBBBGithubAc <imax7964@gmail.com>
2022-05-03 14:18:23 +0530

    sentiment analyzer

--- run.py
+++ run.py
@@ -111,5 +111,5 @@ app.add_middleware(
 
 
 #used for development environment
-if __name__ == "__main__":
-    uvicorn.run("run:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("run:app", host="0.0.0.0", port=8000, reload=True)
